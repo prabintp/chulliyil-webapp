128c6a2d3d83d894221ca33cbd9df764de4f386a
prabin tp <prathazh@publicisgroupe.net>
2024-06-06 15:02:05 +0400

    feat(analytics): setting up vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import "../styles.css";
+import { Analytics } from '@vercel/analytics/react'
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return  <>
+  <Component {...pageProps} />
+  <Analytics />
+</>
 };
 
 export default App;
